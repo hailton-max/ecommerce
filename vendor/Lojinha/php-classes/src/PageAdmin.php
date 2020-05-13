915c6bf31117a5d843f462c74c9a2e8d5a9b4e73
Hailton <hailton.martins@gmail.com>
2020-05-13 18:22:09 -0300

    trabalhando com pageAdmin e tplAdmin

---
+++ vendor/Lojinha/php-classes/src/PageAdmin.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Lojinha;
+
+use Rain\Tpl;
+
+class PageAdmin extends Page{
+
+	public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+	{
+
+		parent::__construct($opts, $tpl_dir);
+	}
+}
+
+ ?>
